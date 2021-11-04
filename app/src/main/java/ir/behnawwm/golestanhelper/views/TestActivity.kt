daacf04407f2c677fe59f12a4a0ba7c02527d65f
Behnam Banaei <behban20@gmail.com>
2021-11-04 14:13:36 +0330

    splash screen done

---
+++ app/src/main/java/ir/behnawwm/golestanhelper/views/TestActivity.kt
@@ -0,0 +1,78 @@
+package ir.behnawwm.golestanhelper.views
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.animation.AlphaAnimation
+import android.view.animation.AnimationUtils
+import androidx.lifecycle.lifecycleScope
+import ir.behnawwm.golestanhelper.R
+import ir.behnawwm.golestanhelper.databinding.ActivityTestBinding
+import ir.behnawwm.golestanhelper.views.main.MainActivity
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import android.animation.Animator
+import android.animation.ArgbEvaluator
+import java.lang.Exception
+import android.animation.ObjectAnimator
+import android.graphics.Color
+
+
+class TestActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityTestBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTestBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+//        binding.tvGolestan1.startAnimation(AlphaAnimation(0F, 1F).apply {
+//            duration = 1000
+//            fillAfter = true
+//        })
+
+//        binding.tvGolestan2.startAnimation(AlphaAnimation(0F, 1F).apply {
+//            duration = 1000
+//            fillAfter = true
+//        })
+        playTextAnimation()
+
+        binding.animationview.addAnimatorListener(object : Animator.AnimatorListener {
+            override fun onAnimationStart(animation: Animator) {
+            }
+
+            override fun onAnimationEnd(animation: Animator) {
+                startActivity(Intent(baseContext, MainActivity::class.java))
+
+            }
+
+            override fun onAnimationCancel(animation: Animator) {
+            }
+
+            override fun onAnimationRepeat(animation: Animator) {
+            }
+        })
+
+    }
+
+    private fun playTextAnimation() {
+        val colorAnim: ObjectAnimator = ObjectAnimator.ofInt(
+            binding.tvGolestan1, "textColor",
+            Color.WHITE, resources.getColor(R.color.blue_f)
+        )
+        colorAnim.setEvaluator(ArgbEvaluator())
+        colorAnim.duration = 2000
+        colorAnim.start()
+
+
+        val colorAnim2: ObjectAnimator = ObjectAnimator.ofInt(
+            binding.tvGolestan2, "textColor",
+            Color.WHITE, resources.getColor(R.color.blue_f)
+        )
+        colorAnim2.setEvaluator(ArgbEvaluator())
+        colorAnim2.duration = 2000
+        colorAnim2.start()
+    }
+}
